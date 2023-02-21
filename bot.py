2a9f02f5016e1423f1961aef9b5792ef493cfed8
Anonym-Guy <104867907+Anonym-Guy@users.noreply.github.com>
2023-02-21 15:13:33 +0100

    Embed, code optimalization and bug fixes
    Requirements.txt now include versions.
    Normal messages have been changed to embeds. Fixes for several bugs including !random, !text and more...

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-from discord.ext.commands import CommandNotFound
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from forex_python.converter import CurrencyRates, RatesNotAvailableError
@@ -11,6 +10,8 @@ import random
 import discord
 import os
 import wikipedia
+
+import embed as emb
 
 # Load discord token (API) from env file
 load_dotenv()
@@ -36,13 +37,15 @@ script_directory = os.path.abspath(os.path.dirname(__file__))  # Get script dire
 
 @bot.command(name='image', help='Send random image to chat')
 async def images(ctx):
-    all_files = os.listdir(script_directory + "/images")  # Get all files from images directory
+    all_files = os.listdir(f"{script_directory}/images")
     allowed_extensions = ('.png', '.jpg', '.jpeg', '.gif')
 
-    images = [file for file in all_files if
-              file.endswith(allowed_extensions)]  # All available images with supported extensions
+    # All available images with supported extensions
+    all_images = [file for file in all_files if
+                  file.endswith(allowed_extensions)]
 
-    random_image = f"{script_directory}/images/{random.choice(images)}"  # Choose random image and add path to it
+    # Choose random image and add path to it
+    random_image = f"{script_directory}/images/{random.choice(all_images)}"
 
     await ctx.send(file=discord.File(random_image))
 
@@ -50,51 +53,54 @@ async def images(ctx):
 # Command to send random text from file text.txt in text directory
 
 @bot.command(name='text', help='Send random text')
-async def text(ctx):
-    text_file_path = f"{script_directory}/text/text.txt"  # Get file path
+async def send_random_text(ctx):
+    text_file_path = f"{script_directory}/text/text.txt"
 
     with open(text_file_path) as file:
-        for line in file.readlines():
-            text = line.rstrip().split(',')  # Split words by comma
+        lines = [line for line in file.read().splitlines()]
 
-    random_text = random.choice(text)  # Choose random text
-    await ctx.send(random_text)
+    # Split text inside line list
+    lines_with_split_text = [line.split(",") for line in lines]
+
+    # Choose from random line random text
+    random_line = random.choice(lines_with_split_text)
+    random_text = random.choice(random_line)
+
+    await emb.send_embed(ctx, title=random_text)
 
 
 # Command to send wikipedia article based on the user input
 
 @bot.command(name='whatis', help='Send wikipedia article about topic you write')
 async def whatis(ctx, *args: str):
-    if len(args) == 0:
-        await ctx.send("Please enter a topic to search for")
-
-    else:
+    if args:
         data = " ".join(args)
-        
+
         # Try if article exist
-        try:  
+        try:
             article = wikipedia.summary(data, sentences=2)
-            await ctx.send(article)
+            await emb.send_embed(ctx, title=data.title(), description=article)
 
         # If article doesn't exist or if exist more articles than 1
-        except (wikipedia.PageError,
-                wikipedia.DisambiguationError):  
+        except (wikipedia.PageError, wikipedia.DisambiguationError):
             articles = wikipedia.search(data, results=5)
 
             # If something found
-            if len(articles) > 0:  
+            if articles:
                 articles = ", ".join(articles)
-                await ctx.send(f"{data} can be : {articles}")
+                await emb.send_embed(ctx, title=f'{data.title()} can be : ', description=articles)
 
             # If article doesn't exist
             else:
-                await ctx.send("The article isn't exist or you wrote it wrong")
+                await emb.send_embed(ctx, title="The article doesn't exist", color=0xFF0000)
+
+    else:
+        await emb.send_embed(ctx, title="Please enter a topic to search for", color=0xFF0000)
 
 
 # Command to make qrcode based on the user input
-
 @bot.command(name='qrcode', help="Make your own qrcode | !qrcode 'what you want in qrcode' ")
-async def qr(ctx, *, data: str = None):
+async def make_qrcode(ctx, *, data: str = None):
     if data:
         qr_code = qr_make(data)  # Make qrcode
 
@@ -104,14 +110,14 @@ async def qr(ctx, *, data: str = None):
         await ctx.send(file=discord.File(qr_filepath))
         os.remove(qr_filepath)  # Delete the qrcode to save space on device
 
-    elif not data:
-        await ctx.send('Please write !qrcode "what do you want in qrcode"')
+    else:
+        await emb.send_embed(ctx, title='Please write !qrcode "what do you want in qrcode"', color=0xFF0000)
 
 
 # Command to send news from www.bbc.com
 
 @bot.command(name='news', help="Send top 3 news")
-async def qr(ctx):
+async def send_three_news(ctx):
     url = 'http://www.bbc.com'
 
     url_to_news = f'{url}/news/world/'
@@ -121,7 +127,7 @@ async def qr(ctx):
     get_promo_news = soup.select("[class~=gs-c-promo-heading]", href=True)
     links = []
 
-    # Add only 3 news to links list with full url
+    # Add only 3 news to list links with full url
     for counter, news in enumerate(get_promo_news):
         news_url = f'{url}{news["href"]}'
 
@@ -138,11 +144,8 @@ async def qr(ctx):
 # Command to rock, paper and scissors game
 
 @bot.command(name='rpas', help="Rock Paper and Scissors game | !rpas 'your choice'")
-async def game(ctx, player_choice: str = None):
-    if not player_choice:
-        await ctx.send(f"Please write !rpas 'your choice'")
-
-    elif player_choice:
+async def rpas_game(ctx, player_choice: str = None):
+    if player_choice:
         choices = ("rock", "paper", "scissors")
         bot_choice = random.choice(choices)
 
@@ -150,52 +153,73 @@ async def game(ctx, player_choice: str = None):
 
             # If tie
             if bot_choice == player_choice:
-                await ctx.send(f"Tie, you both choose {bot_choice}")
+                await emb.send_embed(ctx, title="Tie", description=f"You both choose {bot_choice}", color=0x808080)
 
             # If player win
             elif bot_choice == "rock" and player_choice == "paper" or \
                     bot_choice == "scissors" and player_choice == "rock" or \
                     bot_choice == "paper" and player_choice == "scissors":
 
-                await ctx.send(f"You win, bot choose {bot_choice}\nCongratulations :tada:")
+                await emb.send_embed(ctx,
+                                     title="You win :tada:",
+                                     description=f"Bot choose {bot_choice}",
+                                     color=0x00ff00)
 
             # If bot win
             elif bot_choice == "rock" and player_choice == "scissors" or \
                     bot_choice == "paper" and player_choice == "rock" or \
                     bot_choice == "scissors" and player_choice == "paper":
 
-                await ctx.send(f"Bot win, bot choose {bot_choice}")
+                await emb.send_embed(ctx, title="Bot win", description=f"Bot choose {bot_choice}", color=0xb41b1b)
 
-        elif player_choice not in choices:
-            await ctx.send(f"Write please !rpas (rock, paper, scissors)")
+        else:
+            await emb.send_embed(ctx, title="Please choose only rock, scissors or paper", color=0xFF0000)
+
+    else:
+        await emb.send_embed(ctx, title="Please write !rpas 'your choice'", color=0xFF0000)
 
 
 # Command to send random number in user defined range
 
 @bot.command(name='random', help="Send random number | !random from 'number' to 'number' ")
-async def rnd(ctx, *arg):
-    try:
-        # If user write command in correct format
-        if arg and arg[0] == "from" and arg[2] == "to":
+async def choose_random_number(ctx, *arg):
+    if arg:
+        try:
             # User numbers to integers
             start_number, end_number = int(arg[1]), int(arg[3])
 
-            random_number = random.randrange(start_number, end_number + 1)
+            # If user write command in correct format
+            if arg[0] == "from" and arg[2] == "to" and start_number < end_number:
+                random_number = random.randrange(start_number, end_number + 1)
 
-            await ctx.send(f"Random number is : {random_number}")
+                await emb.send_embed(ctx, title=f'Your random number is {random_number}')
 
-        else:
-            await ctx.send("Write please : !random from 'your number' to 'your number'")
+            elif start_number >= end_number:
+                await emb.send_embed(ctx, title="The first number should be smaller than the second", color=0xFF0000)
 
-    except ValueError:
-        await ctx.send("Write numbers please")
+            else:
+                await emb.send_embed(ctx, title='Write please : !random from "your number" to "your number"',
+                                     color=0xFF0000)
+
+        except ValueError:
+            await emb.send_embed(ctx, title="Write numbers please", color=0xFF0000)
+
+        except IndexError as e:
+            if str(e) == "tuple index out of range":
+                await emb.send_embed(ctx, title='Write please : !random from "your number" to "your number"',
+                                     color=0xFF0000)
+
+    else:
+        await emb.send_embed(ctx, title='Write please : !random from "your number" to "your number"',
+                             color=0xFF0000)
 
 
 # Command to guess the number game
 
 @bot.command(name='gtn', help='Guess the number game')
 async def guess_the_number(ctx):
-    await ctx.send("Guess the number between 1 - 100")
+    await emb.send_embed(ctx, title="Guess the number between 1 - 100",
+                         description='Write the number in the chat. If you want to exit type "exit"')
 
     random_number = random.randrange(1, 101)
     win = False
@@ -207,45 +231,50 @@ async def guess_the_number(ctx):
             message = await bot.wait_for('message', timeout=15,
                                          check=lambda m: m.author == ctx.author and m.channel.id == ctx.channel.id)
 
+            # If user wants to exit the game
+            if message.content == "exit":
+                await emb.send_embed(ctx, title="Thank you for playing")
+                break
+
             # User input to int
             user_guess = int(message.content)
 
             if user_guess == random_number:
                 win = True
-                await ctx.send(f"Congratulations, you guessed the number on {attempts} try :tada:")
+
+                await emb.send_embed(ctx,
+                                     title="Congratulations",
+                                     description=f"You guessed the number on {attempts} try :tada:",
+                                     color=0x00ff00)
                 break
 
             elif user_guess > random_number:
                 attempts += 1
-                await ctx.send("The number is lower")
+                await emb.send_embed(ctx, title="The number is lower")
                 continue
 
             elif user_guess < random_number:
                 attempts += 1
-                await ctx.send("The number is higher")
+                await emb.send_embed(ctx, title='The number is higher')
                 continue
 
         # If user input something else than number
         except ValueError:
-            await ctx.send("Write number please")
+            await emb.send_embed(ctx, title='Write number please', color=0xFF0000)
             continue
 
         # If time is up
         except TimeoutError:
-            await ctx.send("Time is up !")
+            await emb.send_embed(ctx, title='Time is up !', color=0xFF0000)
             break
 
 
 # Command to convert currency to another currency based on the user input
 
 @bot.command(name='convert', help='Convert currency to another currency | !convert from currency to currency amount')
-async def convertor(ctx, from_currency: str = None, to_currency: str = None, amount: str = None):
+async def convert_currency(ctx, from_currency: str = None, to_currency: str = None, amount: str = None):
     try:
-
-        if not from_currency or not to_currency or not amount:
-            await ctx.send('Please write !convert "from currency" "to currency" "amount"')
-
-        elif from_currency and to_currency and amount:
+        if from_currency and to_currency and amount:
 
             # Change user input to upper case
             from_currency, to_currency = from_currency.upper(), to_currency.upper()
@@ -260,39 +289,44 @@ async def convertor(ctx, from_currency: str = None, to_currency: str = None, amo
             # Specified number of decimals
             result = round(convert_result, 2)
 
-            await ctx.send(f"{amount} {from_currency} = {result} {to_currency}")
+            await emb.send_embed(ctx, title=f"{amount} {from_currency} = {result} {to_currency}")
+
+        else:
+            await emb.send_embed(ctx, title='Please write !convert "from currency" "to currency" "amount"',
+                                 color=0xFF0000)
 
     except ValueError:
-        await ctx.send("You entered wrong amount to convert")
+        await emb.send_embed(ctx, title='You entered wrong amount to convert', color=0xFF0000)
 
     except RatesNotAvailableError:
-        await ctx.send("You entered bad currency, or currency is not supported")
+        await emb.send_embed(ctx, title='You entered bad currency, or currency is not supported', color=0xFF0000)
 
 
 # Listen for banned messages from the file words.txt in ban words directory
 
 @bot.listen('on_message')
-async def chat(msg):
-    message_content = msg.content.lower()
+async def check_for_banned_words(msg):
+    message_content = msg.content.casefold()
 
-    text_file_path = f"{script_directory}/ban words/words.txt"  # Get file path
-    warn_message = msg.author.mention + " was warned"
+    text_file_path = f"{script_directory}/ban words/words.txt"
+    warn_message = f"{msg.author.mention} was warned"
 
+    # Get banned words from words.txt
     with open(text_file_path) as file:
-        banned_words = [line for line in file.readline().split(',')]  # Get banned words from words.txt
+        banned_words = [line for line in file.readline().split(',')]
 
     user_bad_words = [word for word in banned_words if word in message_content]
 
-    if any(user_bad_words):  # If user sent bad word
-        await msg.channel.send(
-            warn_message) and await msg.delete()  # Send warn message and delete the message that contains banned words
+    # If user sent bad word
+    if any(user_bad_words):
+        await msg.channel.send(warn_message) and await msg.delete()
 
 
 # Listen for interactions with bot and response
 
 @bot.listen('on_message')
-async def chat(msg):
-    message_content = msg.content.lower()
+async def chat_with_bot(msg):
+    message_content = msg.content.casefold()
 
     # All possible chat interactions variables
     greetings = ("hi", "hello", "was-sup", "sup")
@@ -319,7 +353,8 @@ async def chat(msg):
         await msg.channel.send(f"I am good, and you ?")
 
         # Wait for user message how he is doing
-        response = await bot.wait_for('message', timeout=30, check=lambda m: m.author == msg.author)
+        response = await bot.wait_for('message', timeout=30,
+                                      check=lambda m: m.author == msg.author and m.channel.id == msg.channel.id)
 
         if response.content in user_bad_mood_msg:
             await msg.channel.send(
@@ -332,23 +367,30 @@ async def chat(msg):
 
     # If user asks how old is bot
     if any(user_name_to_react_msg) and any(user_how_old_msg):
-        bot_created = datetime(2022, 8, 5)
+        bot_created = datetime(year=2022, month=8, day=5)
         current_date = datetime.utcnow()
         bot_created, current_date = bot_created.date(), current_date.date()
         age = relativedelta.relativedelta(current_date, bot_created)
 
         await msg.channel.send(
-            f"I was created in 2022-05-08\n"
-            f"So i'm technically {age.years} years {age.months} months {age.days} days old :smile:"
-            )
+            f"I was created in {bot_created}\nSo i'm technically {age.years} years {age.months} months "
+            f"{age.days} days old :smile:"
+        )
 
 
 # If command not found
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send("Command not found !\nIf you want to check all available commands write !help.")
+    if isinstance(error, commands.CommandNotFound):
+        await emb.send_embed(ctx,
+                             title="Command not found !",
+                             description="If you want to check all available commands write !help.",
+                             color=0xFF0000
+                             )
+
+
+# Run bot
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
---
+++ embed.py
@@ -0,0 +1,14 @@
+import discord
+
+
+def send_embed(ctx, title: str, description: str = None, color: int | discord.Colour | None = 0x0391fb):
+    """
+    Send simple discord embed
+    :param ctx: Discord ctx
+    :param str title: The title of embed
+    :param str description: The description of embed
+    :param int,discord.Colour,None color: The color of embed
+    """
+    embed = discord.Embed(title=title, description=description, color=color)
+
+    return ctx.send(embed=embed)
